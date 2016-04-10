ddc4bd59766ec7de14cfffac4d70854f778adf45
tidezyc <tide.zyc@gmail.com>
2016-04-10 15:09:42 +0800

    fix

--- baidupan.php
+++ baidupan.php
@@ -7,7 +7,7 @@ class App
 	function __construct(){
 		$this->_workflows = new workflows();
 	}
-	
+
 	public function getInstance()
 	{
 		if (!self::$_instance instanceof self) {
@@ -23,37 +23,36 @@ class App
 	public function filterDataList($data)
 	{
 		$dataList = array();
-		preg_match_all('/<div class="resitem" >(.*?)<\/div>/', $data, $dataList);
+		preg_match_all('/(<a class="c-blocka" .*?>.*?<\/a>)/', $data, $dataList);
 		return $dataList[1];
 	}
 	public function getData($keyword)
 	{
 		//后面可以在这里加入缓存机制
 		//缓存路径$this->_workflows->cache();
-		$url = 'http://m.baidu.com/s?word=site%3Apan.baidu.com+' . $keyword;
+		$url = 'http://wap.baidu.com/s?word=site%3Apan.baidu.com+' . $keyword;
 		$data = $this->request($url);
 		$num = 1;
 		foreach( $this->filterDataList($data) as $item ) {
-			preg_match('/<span class="date">(.*?)<\/span>/', $item, $time);
-			preg_match('/文件大小:(.*?)分享者/', $item, $desc);
-			$time= " " . str_replace('&#160;', ' ', $time[1]);
-			preg_match('/<a href="(.*?)">(.*?)<\/a>/',$item, $title);
-			$item = str_replace('&#160;', '', $title[2]);
-			$item = str_replace('<em>', '', $item);
-			$item = str_replace('</em>', '', $item);
-			$pos = strpos($item, '_免费高速下载');
-			$item = substr($item, 1, $pos-1);
+			preg_match('/<a .*? href="(.*?)" .*?>.*?<\/a>/',$item, $href);
+			// preg_match('/<span class="date">(.*?)<\/span>/', $item, $time);
+			preg_match('/文件大小:(.*?) /', $item, $size);
+			// $time= " " . str_replace('&#160;', ' ', $time[1]);
+			// $item = str_replace('&#160;', '', $title[2]);
+			// $pos = strpos($item, '_免费高速下载');
+			// $item = substr($item, 1, $pos-1);
+            
+            $title =preg_replace('/<.*?>/', '', $item);            
 
-			$desc = str_replace('&#160;', '', $desc[1]);
-			$desc = str_replace('<em>', '', $desc);
-			$desc = str_replace('</em>', '', $desc);
+			// $desc = str_replace('&#160;', '', $desc[1]);
+			// $desc = str_replace('<em>', '', $desc);
+			// $desc = str_replace('</em>', '', $desc);
+            $href = str_replace('&amp;', '====', $href[1]);
 
-			$href = str_replace('&amp;', '====', $title[1]);
-
-			$this->_workflows->result($num . '.' . time(), $href, $item, '文件大小:' . $desc .'上传时间:' . $time, 'icon.png');
+			$this->_workflows->result($num . '.' . time(), $href, $title, '文件大小:' . $size[1] . $href, 'icon.png');
 			$num ++;
 		}
-		
+
 		return $this->_workflows->toxml();
 	}
 	public function run($query)
@@ -61,5 +60,5 @@ class App
 		return $this->getData($query);
 	}
 }
-#echo App::getInstance()->run('大主宰');
+// echo App::getInstance()->run('三体');
 ?>
